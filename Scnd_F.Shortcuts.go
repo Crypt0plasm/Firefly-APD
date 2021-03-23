45cd366973e33ee42b1503466af8b175cd1d6579
AncientHodler <53853648+AncientHodler@users.noreply.github.com>
2021-03-23 14:27:08 +0100

    Added NFBI short function.

--- Scnd_F.Shortcuts.go
+++ Scnd_F.Shortcuts.go
@@ -1,4 +1,7 @@
 package Firefly_Precision
+
+import "math/big"
+
 //
 //	Scnd_F.Shortcuts.go					Secondary Function Shortcuts
 //
@@ -22,11 +25,12 @@ package Firefly_Precision
 // It has no restriction on precision. Tested Accepted characters are "." and "-" for a negative sign
 func NFS(s string) *Decimal {
 	d := new(Decimal)
-    	//Further Tests can be added to detect wrong string construction.
-    	//It is always assumed the string number used with this function is of correct construction.
-	d,_,_ = NewFromString(s)
+	//Further Tests can be added to detect wrong string construction.
+	//It is always assumed the string number used with this function is of correct construction.
+	d, _, _ = NewFromString(s)
 	return d
 }
+
 //================================================
 //
 // Function 02 - NFI
@@ -37,18 +41,31 @@ func NFS(s string) *Decimal {
 //		 9,223,372,036,854,775,807
 func NFI(coeff int64) *Decimal {
 	d := new(Decimal)
-	d = New(coeff,0)
+	d = New(coeff, 0)
 	return d
 }
+
 //================================================
 //
-// Function 02 - INT64
+// Function 03 - NFBI
+//
+// NFBI creates a new decimal from a big.Int number
+func NFBI(Number *big.Int) *Decimal {
+	d := new(Decimal)
+	d = NewWithBigInt(Number, 0)
+	return d
+}
+
+//================================================
+//
+// Function 04 - INT64
 //
 // INT64 returns the int64 representation of x.
 // No error is returned with this function
 func INT64(Decimal2Convert *Decimal) int64 {
-    var Result int64
-    Result, _ = Decimal2Convert.Int64()
-    return Result
+	var Result int64
+	Result, _ = Decimal2Convert.Int64()
+	return Result
 }
+
 //================================================
